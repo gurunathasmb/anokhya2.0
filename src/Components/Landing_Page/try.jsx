697f05091e8caa98874295eb138cd34388b4b8bd
Gurunathagouda M biradar <gurunathagoudambiradar@gmail.com>
2024-11-07 11:16:13 +0530

    try.jsx

--- src/Components/Landing_Page/try.jsx
+++ src/Components/Landing_Page/try.jsx
@@ -5,7 +5,7 @@ import "./Landing_Page.css";
 //import Alert_mssg from '../../Components/Alert_mssg/Alert_mssg'
 
 import logo from "../../Assets/LPMV/Logo.png";
-import banner from "../../Assets/LPMV/Banner.png";
+import banner from "../../Assets/LPMV/2.gif";
 
 export function BackgroundBoxesDemo() {
   return (
